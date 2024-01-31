dfb54efb08717d35a5a130a4af5ccfc239c141f0
David Q <dvdquerol@gmail.com>
2024-01-31 19:39:20 +0100

    Update module setting in tsconfig.json and refactor code in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,5 @@ import { fetchUserProfile } from "./fetch";
 
 const midudevId = '8242141302';
 
-(async () => {
-    const response = await fetchUserProfile({ userId: midudevId });
-    console.log(response);
-})();
+const response = await fetchUserProfile({ userId: midudevId });
+console.log(response);
