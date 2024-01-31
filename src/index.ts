551d2ee797a73a59a979a9464bc8763883dcf98d
David Q <dvdquerol@gmail.com>
2024-01-31 18:18:21 +0100

    Add GraphQL endpoint and constants

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-fetch ('https://www.threads.net/api/graphql', {
+import { ENDPOINTS_DOCUMENT_ID, GRAPHQL_ENDPOINT, THREADS_APP_ID } from "./consts";
+
+fetch (GRAPHQL_ENDPOINT, {
     method: 'POST',
     headers: {
         'content-type': 'application/x-www-form-urlencoded',
         'sec-fetch-site': 'same-origin',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)',
-        'x-ig-app-id':'238260118697367'
+        'x-ig-app-id': THREADS_APP_ID
     },
-    body: 'variables={ "userID": "8242141302" }&doc_id=23996318473300828'
+    body: `variables={ "userID": "8242141302" }&doc_id=${ENDPOINTS_DOCUMENT_ID.USER_PROFILE}`
     })
 .then(res => res.json())
 .then(res => console.log(res))
