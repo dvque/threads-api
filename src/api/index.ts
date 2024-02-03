b1c521aab6ca478eaed39a1fd2374adafd96a205
David Q <dvdquerol@gmail.com>
2024-02-03 10:29:42 +0100

    Hono API endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,27 +1,43 @@
 import { Hono } from "hono";
 import { fetchUserProfile } from "../lib/fetch";
 
-const server = Bun.serve({
-  port: 1234,
-  async fetch(request) {
-    const { pathname, searchParams } = new URL(request.url);
+const port = +(Bun.env.PORT || 1234);
 
-    if(pathname.startsWith("/api/users/")) {
-      const userId = searchParams.get("userId")!;
-      const userName = searchParams.get("userName")!;
+const app = new Hono()
 
-      const data = await fetchUserProfile({ userId: userId, userName: userName })
-
-      return new Response(JSON.stringify(data), {
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-    }
-
-    return new Response(null, { status: 404 });
-
-  },
+app.get('/api/users/:userId', async (context) => {
+  const userId = context.req.param('userId')
+  const data = await fetchUserProfile({ userId })
+  return context.json(data)
 })
 
-console.log(`Listening on localhost:${server.port}`)
+export default {
+  port,
+  fetch: app.fetch, // Bun
+}
+
+// Bun API
+// const server = Bun.serve({
+//   port: 1234,
+//   async fetch(request) {
+//     const { pathname, searchParams } = new URL(request.url);
+
+//     if(pathname.startsWith("/api/users/")) {
+//       const userId = searchParams.get("userId")!;
+//       const userName = searchParams.get("userName")!;
+
+//       const data = await fetchUserProfile({ userId: userId, userName: userName })
+
+//       return new Response(JSON.stringify(data), {
+//         headers: {
+//           "content-type": "application/json",
+//         },
+//       });
+//     }
+
+//     return new Response(null, { status: 404 });
+
+//   },
+// })
+
+// console.log(`Listening on localhost:${server.port}`)
