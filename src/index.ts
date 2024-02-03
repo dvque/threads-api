df3b9c8d6ad0d5e8103e80416ac2f0e9c473ccff
David Q <dvdquerol@gmail.com>
2024-02-03 09:19:17 +0100

    API Bun.serve

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-
-import { fetchUserIdByName, fetchUserProfile, fetchUserThreads } from "./fetch";
-
-const midudevId = '8242141302';
-
-const response = await fetchUserThreads({ userName: 'midu.dev' });
-console.log(response);
